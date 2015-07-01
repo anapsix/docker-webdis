19504ec9e59a49ebda2b93ced0c6cbe64acc4dcf
Anastas Dancha <anapsix@random.io>
2015-07-01 09:55:52 -0400

    ENTRYPOINT: fix Tutum :P

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 set -e
 
+tutum_compat() {
+  if [ -n "$REDIS_1_ENV_TUTUM_IP_ADDRESS" ]; then
+    echo "${REDIS_1_ENV_TUTUM_IP_ADDRESS%/*}  redis" >> /etc/hosts
+  fi
+}
+
+tutum_compat || true
+
 if [ $# -eq 0 ]; then
   echo "starting webdis.." >&2
   set -- webdis /etc/webdis.json
